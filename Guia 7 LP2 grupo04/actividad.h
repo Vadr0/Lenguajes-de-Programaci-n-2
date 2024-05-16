1ace27721e0b4b71b7288aaa240edfb52318ee5b
rodel2A <164429797+rodel2A@users.noreply.github.com>
2024-05-16 18:02:10 -0500

    Update actividad.h

--- Guia 7 LP2 grupo04/actividad.h
+++ Guia 7 LP2 grupo04/actividad.h
@@ -9,7 +9,6 @@ protected:
     string apellido;
     string direccion;
     string numeroTarjeta;
-    // Método protegido para encriptar la información
     void encriptarInformacion() {
         // Encriptar el nombre y el apellido
         for (size_t i = 0; i < nombre.size(); ++i) {        //el size es un metodo de la libreria string 
@@ -24,15 +23,13 @@ protected:
                 c = ((c - 'a' + 1) % 26) + 'a';
             }
         }
-        // Encriptar la dirección
-        for (size_t i = 0; i < direccion.size(); ++i) {
+        for (size_t i = 0; i < direccion.size(); ++i) {  // Encriptar la dirección
             char& c = direccion[i];
             if (c >= 'a' && c <= 'z') {
                 c = ((c - 'a' + 1) % 26) + 'a';
             }
         }
-        // Encriptar el número de tarjeta
-        for (size_t i = 0; i < numeroTarjeta.size(); ++i) {
+        for (size_t i = 0; i < numeroTarjeta.size(); ++i) { // Encriptar el número de tarjeta
             char& c = numeroTarjeta[i];
             if (c >= '0' && c <= '9') {
                 c = ((c - '0' + 1) % 10) + '0';
@@ -41,13 +38,10 @@ protected:
         cout << "Información encriptada correctamente." << endl;
     }
 public:
-    // Constructor
     Cliente(string _nombre, string _apellido, string _direccion, string _numeroTarjeta)
         : nombre(_nombre), apellido(_apellido), direccion(_direccion), numeroTarjeta(_numeroTarjeta) {}
-    // Destructor virtual
     virtual ~Cliente() {}
-    // Método para mostrar la información del cliente
-    void mostrarInformacion() {
+    void mostrarInformacion() {          //mostrar
         cout << "Nombre: " << nombre << endl;
         cout << "Apellido: " << apellido << endl;
         cout << "Dirección: " << direccion << endl;
@@ -55,28 +49,21 @@ public:
     }
 };
 
-// Clase derivada ClienteSeguro utilizando herencia protegida
-class ClienteSeguroProtegida : protected Cliente {
+class ClienteSeguroProtegida : protected Cliente {       //se hereda o se deriba  
 public:
-    // Constructor que llama al constructor de la clase base y que inicializa los atributos adicionales
     ClienteSeguroProtegida(string _nombre, string _apellido, string _direccion, string _numeroTarjeta)
         : Cliente(_nombre, _apellido, _direccion, _numeroTarjeta) {}
-    // Método público para mostrar la información del cliente
     void mostrarInformacionCliente() {
-        // Acceso a los miembros protegidos de la clase base
         cout << "Información del cliente:" << endl;
         cout << "Nombre: " << nombre << endl;
         cout << "Apellido: " << apellido << endl;
         cout << "Dirección: " << direccion << endl;
         cout << "Número de tarjeta: " << numeroTarjeta << endl;
     }
-    // Método para encriptar la información del cliente (llama al método protegido de la clase base)
-    void encriptarInformacionCliente() {
+    void encriptarInformacionCliente() {     //metodo para encriptar la informacion de la clase base
         encriptarInformacion();
     }
-    // Método para desencriptar la información del cliente
-    void desencriptarInformacionCliente() {
-        // Desencriptar el nombre y el apellido
+    void desencriptarInformacionCliente() {     //metodo para desencriptar
         for (size_t i = 0; i < nombre.size(); ++i) {
             char& c = nombre[i];
             if (c >= 'a' && c <= 'z') {
@@ -89,14 +76,12 @@ public:
                 c = ((c - 'a' - 1 + 26) % 26) + 'a';
             }
         }
-        // Desencriptar la dirección
         for (size_t i = 0; i < direccion.size(); ++i) {
             char& c = direccion[i];
             if (c >= 'a' && c <= 'z') {
                 c = ((c - 'a' - 1 + 26) % 26) + 'a';
             }
         }
-        // Desencriptar el número de tarjeta
         for (size_t i = 0; i < numeroTarjeta.size(); ++i) {
             char& c = numeroTarjeta[i];
             if (c >= '0' && c <= '9') {
@@ -108,19 +93,13 @@ public:
 };
 
 int main() {
-    // Crear un objeto de la clase ClienteSeguro utilizando herencia protegida
     ClienteSeguroProtegida clienteProtegido("Juan", "Perez", "Parra 1245", "679416522");
-    // Mostrar la información del cliente antes de encriptar
     cout << "Información del cliente antes de encriptar:" << endl;
     clienteProtegido.mostrarInformacionCliente();
-    // Encriptar la información del cliente
     clienteProtegido.encriptarInformacionCliente();
-    // Mostrar la información del cliente después de encriptar
     cout << "\nInformación del cliente después de encriptar:" << endl;
     clienteProtegido.mostrarInformacionCliente();
-    // Desencriptar la información del cliente
     clienteProtegido.desencriptarInformacionCliente();
-    // Mostrar la información del cliente después de desencriptar
     cout << "\nInformación del cliente después de desencriptar:" << endl;
     clienteProtegido.mostrarInformacionCliente();
     return 0;
