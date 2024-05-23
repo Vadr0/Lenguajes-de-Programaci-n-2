f56e4d348beb61a1c46e8ed3854737e8cb38a6b7
Orlando Jose  Montenegro Gonzales <164653615+TrabajosProgrmacion@users.noreply.github.com>
2024-05-23 10:18:50 -0500

    Update actividad.h

--- Guia 7 LP2 grupo04/actividad.h
+++ Guia 7 LP2 grupo04/actividad.h
@@ -165,3 +165,107 @@ if __name__ == "__main__":
 
     print("\nInformación del cliente después de desencriptar:")
     cliente_protegido.mostrar_informacion_cliente()
+
+
+Codigo mejoradi
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Declaración anticipada del prototipo de la función burbuja
+void burbuja(string& str);
+
+// Clase base Cliente
+class Cliente {
+protected:
+    string nombre;
+    string apellido;
+    string direccion;
+    string numeroTarjeta;
+
+    // Método protegido para encriptar información
+    void encriptar(string& str) {
+        burbuja(str);
+    }
+
+public:
+    Cliente(string _nombre, string _apellido, string _direccion, string _numeroTarjeta)
+        : nombre(_nombre), apellido(_apellido), direccion(_direccion), numeroTarjeta(_numeroTarjeta) {}
+
+    virtual ~Cliente() {}
+
+    void mostrarInformacion() {
+        cout << "Nombre: " << nombre << endl;
+        cout << "Apellido: " << apellido << endl;
+        cout << "Dirección: " << direccion << endl;
+        cout << "Número de tarjeta: " << numeroTarjeta << endl;
+    }
+};
+
+// Clase derivada con herencia pública
+class ClienteSeguro : public Cliente {
+private:
+    string originalNombre;
+    string originalApellido;
+    string originalDireccion;
+    string originalNumeroTarjeta;
+public:
+    ClienteSeguro(string _nombre, string _apellido, string _direccion, string _numeroTarjeta)
+        : Cliente(_nombre, _apellido, _direccion, _numeroTarjeta), originalNombre(_nombre), originalApellido(_apellido), originalDireccion(_direccion), originalNumeroTarjeta(_numeroTarjeta) {}
+
+    void mostrarInformacionCliente() {
+        cout << "Información del cliente:" << endl;
+        mostrarInformacion();
+    }
+
+    void encriptarInformacionCliente() {
+        encriptar(nombre);
+        encriptar(apellido);
+        encriptar(direccion);
+        encriptar(numeroTarjeta);
+        cout << "Información encriptada correctamente." << endl;
+    }
+
+    void desencriptarInformacionCliente() {
+        nombre = originalNombre;
+        apellido = originalApellido;
+        direccion = originalDireccion;
+        numeroTarjeta = originalNumeroTarjeta;
+        cout << "Información desencriptada correctamente." << endl;
+    }
+};
+
+// Función burbuja para ordenar caracteres en una cadena
+void burbuja(string& str) {
+    int n = str.length();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (str[j] > str[j + 1]) {
+                char temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    ClienteSeguro clienteProtegido("Juan Lopez", "Perez del Campo", "Urb:Callao Calle 123", "76251734");
+    
+    cout << "Información del cliente antes de encriptar:" << endl;
+    clienteProtegido.mostrarInformacionCliente();
+    
+    clienteProtegido.encriptarInformacionCliente();
+    
+    cout << "\nInformación del cliente después de encriptar:" << endl;
+    clienteProtegido.mostrarInformacionCliente();
+    
+    clienteProtegido.desencriptarInformacionCliente();
+    
+    cout << "\nInformación del cliente después de desencriptar:" << endl;
+    clienteProtegido.mostrarInformacionCliente();
+    
+    return 0;
+}
+
+
