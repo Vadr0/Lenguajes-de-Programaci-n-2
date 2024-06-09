af3621e2dd5219b842598b894e0079bcd3cfa4db
rodel2A <164429797+rodel2A@users.noreply.github.com>
2024-06-09 13:57:23 -0500

    Update and rename Actividad N3.cpp to Actividad 3.cpp

--- Lab 9 LP2 Grupo 04/Actividad 3.cpp
+++ Lab 9 LP2 Grupo 04/Actividad 3.cpp
@@ -4,36 +4,24 @@ using namespace std;
 
 class Calculadora {
 public:
-    // Suma de dos enteros
-    int sumar(int a, int b) {
+    int sumar(int a, int b) {   // Suma de dos enteros
         return a + b;
     }
-
-    // Suma de dos números de punto flotante
-    double sumar(double a, double b) {
+    double sumar(double a, double b) {  // Suma de dos números de punto flotante
         return a + b;
     }
-
-    // Suma de tres enteros
-    int sumar(int a, int b, int c) {
+    int sumar(int a, int b, int c) {        // Suma de tres enteros
         return a + b + c;
     }
 };
 
 int main() {
     Calculadora calc;
-
-    // Llamada al método sumar(int, int)
     int resultado1 = calc.sumar(3, 4);
     cout << "Suma de 3 y 4 (int): " << resultado1 << endl;
-
-    // Llamada al método sumar(double, double)
     double resultado2 = calc.sumar(2.5, 3.5);
     cout << "Suma de 2.5 y 3.5 (double): " << resultado2 << endl;
-
-    // Llamada al método sumar(int, int, int)
     int resultado3 = calc.sumar(1, 2, 3);
     cout << "Suma de 1, 2 y 3 (int): " << resultado3 << endl;
-
     return 0;
 }
